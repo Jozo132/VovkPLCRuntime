676f5e7a41e43999936de7e93fbdb401ed809fad
J.Vovk <jozo132@gmail.com>
2022-12-12 10:43:40 +0100

    Small visual changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
-#include <avr/wdt.h>
-
+// Main file for testing the VovkPLCRuntime library
 
 #define __RUNTIME_TEST__ // Avoid using this with microcontrollers with limited RAM!
 #define __RUNTIME_TEST_ADVANCED_DEBUGGING__ // Avoid using this with microcontrollers with limited RAM! This makes the output logs more verbose
+
 #include <VovkPLCRuntime.h>
 
-
 void setup() {
-  wdt_disable();
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   while (!Serial) delay(10); // wait for serial port to connect. Needed for native USB port only
 
-  runtime_test(); // Start the runtime tests
+  // Start the runtime tests
+  runtime_test();
+
+  // Blink the LED to indicate that the tests are done
+  while (1) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(500);
+  }
 }
 
-int cycle = 0;
 void loop() {
-  cycle++;
-  digitalWrite(LED_BUILTIN, cycle % 2);
-  delay(500);
+  // Nothing to do here
 }
