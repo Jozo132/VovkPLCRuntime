fcc2b58ac5f8c2dadcc81e31d1e7ccd65f862790
J.Vovk <jozo132@gmail.com>
2023-07-24 20:56:26 +0200

    Added GPLv3 notice

--- lib/VovkPLCRuntime/src/tools/assembly/wasm/jvmalloc.h
+++ lib/VovkPLCRuntime/src/tools/assembly/wasm/jvmalloc.h
@@ -1,10 +1,31 @@
-// jvmalloc - J.Vovk memory allocation implementation for wasm without dependencies
+// jvmalloc.h - 1.0.0 - 2022-12-11
+//
+// Copyright (c) 2022 J.Vovk
+//
+// This file is part of VovkPLCRuntime.
+//
+// VovkPLCRuntime is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VovkPLCRuntime is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VovkPLCRuntime.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+// jvmalloc - memory allocation implementation for wasm without dependencies
 // This is a simple implementation of malloc/free that uses a single global memory pool that is allocated at startup.
 // The code is by no means fast or efficient, but it is simple and it just works.
 // It is designed to be used on the web or in node.js.
 // It exports 'malloc' and 'free' symbols along with some string functions.
-
-#pragma once
 
 #ifdef __WASM__
 #include "jvint.h"
