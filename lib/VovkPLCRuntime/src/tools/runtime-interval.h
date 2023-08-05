02199279f8e474a4e3cdba215089245596de5dc5
J.Vovk <jozo132@gmail.com>
2023-08-05 14:07:27 +0200

    Added missing license header

--- lib/VovkPLCRuntime/src/tools/runtime-interval.h
+++ lib/VovkPLCRuntime/src/tools/runtime-interval.h
@@ -1,3 +1,24 @@
+// runtime-interval.h - 1.0.0 - 2023-08-05
+//
+// Copyright (c) 2023 J.Vovk
+//
+// This file is part of VovkPLCRuntime.
+//
+// VovkPLCRuntime is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// VovkPLCRuntime is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with VovkPLCRuntime.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #pragma once
 
 uint32_t interval_millis_now = 0;
