63428a6a0e5ef79edfcab03d4188ac423ace822a
Jože Vovk <jozo132@gmail.com>
2025-02-20 01:04:44 +0100

    Added blinky example arduino file

---
+++ lib/VovkPLCRuntime/examples/blinky/blinky.ino
@@ -0,0 +1,72 @@
+#define __RUNTIME_PRODUCTION__
+#define PLCRUNTIME_SERIAL_ENABLED
+
+#define PLCRUNTIME_NUM_OF_OUTPUTS   10
+#define PLCRUNTIME_INPUT_OFFSET     10
+#define PLCRUNTIME_MAX_STACK_SIZE   64
+#define PLCRUNTIME_MAX_MEMORY_SIZE  64
+#define PLCRUNTIME_MAX_PROGRAM_SIZE 1024
+
+#include <VovkPLCRuntime.h>
+VovkPLCRuntime runtime;
+
+#define BUTTON_PIN 12
+#define LED_PIN LED_BUILTIN
+
+void setup() {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+    runtime.initialize();
+
+    // input  = I0.0  (location 10.0 in the memory map)
+    // output = Q0.0  (location 20.0 in the memory map)
+    //
+    // LADDER diagram:
+    /*
+     *     P_1s     output
+     *   ---| |------(  )---
+     *      1.4      Q0.0
+    **/
+    // PLCASM code:
+    /* 
+     *       u8.readBit     1.4    // Read bit 1.4 which is 1s pulse
+     *       jump_if_not    end    // Jump to the label 'end' if the bit is OFF
+     *       u8.writeBitInv 20.0   // Invert bit 20.0
+     *   end:                      // Label to jump to
+    **/
+    // Compiled bytecode:
+    // Program[10] [5C 00 01 E2 00 09 78 00 14 FF]
+    // Explanation:
+    /*
+     *  5C 00 01  ->  read u8 bit 4 at address 0x0001 and put it on the stack
+     *  E2 00 09  ->  jump to address 0x0009 if the value from the stack is OFF
+     *  78 00 14  ->  toggle u8 bit 0 at address 0x0014
+     *  FF        ->  end of program, which is also the address 0x0009 to jump to
+    **/
+    const u8 program [] = {
+        0x5C, 0x00, 0x01,
+        0xE2, 0x00, 0x09,
+        0x78, 0x00, 0x14,
+        0xFF,
+    };
+    const u16 size = sizeof(program);
+    // Load the program into the runtime without checksum verification, assuming the program is correct
+    runtime.loadProgramUnsafe(program, size);
+}
+
+void loop() {
+    // ######### 1. Read inputs #########
+    bool input = !digitalRead(BUTTON_PIN);
+    runtime.setInputBit(0, 0, input);
+
+    // ######### 2. Run the PLC program #########
+    runtime.run();
+
+    // ######### 3. Write outputs #########
+    bool output = runtime.getOutputBit(0, 0);
+    digitalWrite(LED_PIN, !output);
+
+    // ######### 4. Listen for remote commands #########
+    runtime.listen();
+}
