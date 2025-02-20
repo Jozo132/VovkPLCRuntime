989a7a49c1a95c7309693c389a442fb2238a2590
Jože Vovk <jozo132@gmail.com>
2025-02-20 01:05:59 +0100

    Refactor blinky.asm sample

--- wasm/web-server-test/src/samples/blinky.asm
+++ wasm/web-server-test/src/samples/blinky.asm
@@ -1,6 +1,5 @@
 
-// Named constant
-const marker = 10.0
-
-// Invert bit
-u8.writeBitInv marker
+    u8.readBit     1.4    // Read bit 1.4 which is 1s pulse
+    jump_if_not    end    // Jump to the label 'end' if the bit is OFF
+    u8.writeBitInv 20.0   // Invert bit 20.0
+end:                      // Label to jump to
