0ba70861fd8a8b2917ff72701c0c75de7d52968f
Jože Vovk <jozo132@gmail.com>
2025-03-10 18:25:59 +0100

    Update blinky.ino to correct memory map locations and adjust output logic

--- examples/blinky/blinky.ino
+++ examples/blinky/blinky.ino
@@ -19,35 +19,35 @@ void setup() {
     Serial.begin(115200);
     runtime.initialize();
 
-    // input  = I0.0  (location 10.0 in the memory map)
-    // output = Q0.0  (location 20.0 in the memory map)
+    // input  = I0.0  (location 16.0 in the memory map)
+    // output = Q0.0  (location 32.0 in the memory map)
     //
     // LADDER diagram:
     /*
      *     P_1s     output
      *   ---| |------(  )---
-     *      1.4      Q0.0
+     *      2.4      Q0.0
     **/
     // PLCASM code:
     /*
-     *       u8.readBit     1.4    // Read bit 1.4 which is 1s pulse
+     *       u8.readBit     2.4    // Read bit 2.4 which is 1s pulse
      *       jump_if_not    end    // Jump to the label 'end' if the bit is OFF
-     *       u8.writeBitInv 20.0   // Invert bit 20.0
+     *       u8.writeBitInv 32.0   // Invert bit Q0.0 (Outputs start at 0x20 -> 32)
      *   end:                      // Label to jump to
     **/
     // Compiled bytecode:
-    // Program[10] [5C 00 01 E2 00 09 78 00 14 FF]
+    // Program[10] [5C 00 02 E2 00 09 78 00 20 FF]
     // Explanation:
     /*
-     *  5C 00 01  ->  read u8 bit 4 at address 0x0002 and put it on the stack
+     *  5C 00 02  ->  read u8 bit 4 at address 0x0002 and put it on the stack
      *  E2 00 09  ->  jump to address 0x0009 if the value from the stack is OFF
-     *  78 00 14  ->  toggle u8 bit 0 at address 0x0014
+     *  78 00 20  ->  toggle u8 bit 0 at address 0x0020
      *  FF        ->  end of program, which is also the address 0x0009 to jump to
     **/
     const u8 program [] = {
         0x5C, 0x00, 0x02,
         0xE2, 0x00, 0x09,
-        0x78, 0x00, 0x14,
+        0x78, 0x00, 0x20,
         0xFF,
     };
     const u16 size = sizeof(program);
@@ -65,7 +65,7 @@ void loop() {
 
     // ######### 3. Write outputs #########
     bool output = runtime.getOutputBit(0, 0);
-    digitalWrite(LED_PIN, !output);
+    digitalWrite(LED_PIN, output);
 
     // ######### 4. Listen for remote commands #########
     runtime.listen();
